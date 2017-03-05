4994a93336de499977fe56a31dcdc69aad9bbfa0
galuhestya <galuhestya@gmail.com>
2017-03-05 17:42:20 +0700

    migration

---
+++ database/migrations/2017_03_05_103000_create_claim_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClaimTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::enableForeignKeyConstraints();
+        Schema::create('claims', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('claim_type');
+            $table->string('claim_data_id');
+            $table->integer('claimer_id')->unsigned();
+            $table->foreign('claimer_id')->references('id')
+              ->on('users')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+            $table->integer('approver_id')->unsigned();
+            $table->foreign('approver_id')->references('id')
+              ->on('users')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+            $table->integer('finance_id')->unsigned();
+            $table->foreign('finance_id')->references('id')
+              ->on('users')
+              ->onUpdate('cascade')
+              ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
