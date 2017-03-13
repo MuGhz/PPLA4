73fedf57c5d870a8d56eb60de545758290d2d764
galuhestya <galuhestya@gmail.com>
2017-03-13 16:59:29 +0700

    benerin seed

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -21,7 +21,7 @@ class UserTableSeeder extends Seeder
             \App\User::create([
               "name" => "admin$j$i",
               "name" => "admin$j$i",
-              "email" => "admin$j$i@$company[$i]->name.com",
+              "email" => "admin$j$i@".$company[$i]->name.".com",
               "password" => bcrypt("admin$j$i"),
               "role" => "admin",
               "company" => $company[$i]->id,
@@ -29,7 +29,7 @@ class UserTableSeeder extends Seeder
             \App\User::create([
               "name" => "claimer$j$i",
               "name" => "claimer$j$i",
-              "email" => "claimer$j$i@$company[$i]->name.com",
+              "email" => "claimer$j$i@".$company[$i]->name.".com",
               "password" => bcrypt("claimer$j$i"),
               "role" => "claimer",
               "company" => $company[$i]->id,
@@ -37,7 +37,7 @@ class UserTableSeeder extends Seeder
             \App\User::create([
               "name" => "approver$j$i",
               "name" => "approver$j$i",
-              "email" => "approver$j$i@$company[$i]->name.com",
+              "email" => "approver$j$i@".$company[$i]->name.".com",
               "password" => bcrypt("approver$j$i"),
               "role" => "approver",
               "company" => $company[$i]->id,
@@ -45,7 +45,7 @@ class UserTableSeeder extends Seeder
             \App\User::create([
               "name" => "finance$j$i",
               "name" => "finance$j$i",
-              "email" => "finance$j$i@$company[$i]->name.com",
+              "email" => "finance$j$i@".$company[$i]->name.".com",
               "password" => bcrypt("finance$j$i"),
               "role" => "finance",
               "company" => $company[$i]->id,
