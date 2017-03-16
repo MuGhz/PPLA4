5fdd0a8b7904ec58e4f22f77dd92b7ca5e116721
galuhestya <galuhestya@gmail.com>
2017-03-16 13:49:37 +0700

    coba form tiket

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,3 @@
 @extends('adminlte::layouts.landing')
 
+<iframe src="https://sandbox.tiket.com/widget/multi_searchbox?business=22693559&language=id&size_type=normal&position=header-top&product_type=hotel" style="width: 800px; height: 800px;" scrolling="no" frameborder="0"></iframe>
