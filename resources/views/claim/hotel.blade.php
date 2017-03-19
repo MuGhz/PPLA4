851fbde366a74debe9957e730995d2eab9d14bc7
galuhestya <galuhestya@gmail.com>
2017-03-19 19:31:07 +0700

    routing, pasang widget, widget bakal dilepas

---
+++ resources/views/claim/hotel.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="tiket-root" data-widget="tiket-boxsearchwidget" data-businessid="22693559" data-lang="id" data-size_type="normal" data-width="800" data-height="800" data-position="middle-content" data-product_type="hotel">
+  </div>
+</div>
+@endsection
+@section('js')
+  <script type="text/javascript" src="https://sandbox.tiket.com/js/new_widget/tiket_widgetframe_v3.js" async="true"></script>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,1 @@
 @extends('adminlte::layouts.landing')
-
-<iframe src="https://sandbox.tiket.com/widget/multi_searchbox?business=22693559&language=id&size_type=normal&position=header-top&product_type=hotel" style="width: 800px; height: 800px;" scrolling="no" frameborder="0"></iframe>
