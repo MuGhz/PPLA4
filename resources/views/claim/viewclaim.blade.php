a7402bb0d257649d4000d5c3acc406013e2cebb7
Muhammad Ghozi <Muhammad Ghozi>
2017-03-28 14:38:21 +0700

    perubahan kecil di detail claim

--- resources/views/claim/viewclaim.blade.php
+++ resources/views/claim/viewclaim.blade.php
@@ -11,44 +11,10 @@ $token = $value -> claim_data_id ;
 	<center><h3 class="box-title">Detail Claim</h3></center>
 	<div class="row">
       <div class="form-group col-md-8">
-        <label>Type: </label>
+        <label>Type: Hotel</label>
       </div>
     </div>
-	<div class="row">
-      <div class="form-group col-md-8">
-        <label>Title: </label>
-      </div>
-    </div>
-	<div class="row">
-      <div class="form-group col-md-8">
-        <label>Description: </label>
-      </div>
-    </div>
-	<div class="row">
-      <div class="form-group col-md-8">
-        <label>Date: </label>
-      </div>
-    </div>
-	<div class="row">
-      <div class="form-group col-md-8">
-        <label>From - To: </label>
-      </div>
-    </div>
-	<div class="row">
-      <div class="form-group col-md-8">
-        <label>Passenger: </label>
-      </div>
-    </div>
-	<div class="row">
-      <div class="form-group col-md-8">
-        <label>Status: </label>
-      </div>
-    </div>
-	<div class="row">
-      <div class="form-group col-md-8">
-        <label>Notes: </label>
-      </div>
-    </div>
+	
 	<div class="row">
       <div class="form-group col-md-8">
         <div class="form-group col-md-4">
