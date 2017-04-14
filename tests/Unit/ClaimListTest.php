7a6622d1c3bf6b6738eb272243e85bd1244b8bb5
ZakyKh <mzakykhairuddin@gmail.com>
2017-04-14 20:49:03 +0700

    Add test for HomeController@index (WIP)

---
+++ tests/Unit/ClaimListTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Http\Controllers\HomeController;
+use App\Company;
+use App\Claim;
+use App\User;
+
+class ClaimListTest extends TestCase
+{
+	use DatabaseTransactions;
+	
+	public function setUp()
+	{
+		parent::setUp();
+	}
+	
+    public function testViewHasData()
+    {
+		$company = factory(Company::class)->create(['name' => 'Test Company']);
+		$user = factory(User::class)->create([
+			'name' => 'Test User',
+			'email' => 'Test_User_Email@testdomain.test',
+			'password' => 'Test User Password',
+			'role' => 'claimer',
+			'company' => $company->id
+		]);
+		
+		// $response = $this->actingAs($user)
+						 // ->withSession(['user' => 'user'])
+						 // ->get('/home');
+		
+		$hc = new HomeController();
+		$response = $hc->index();
+		$response = $this->createTestResponse($response)
+						 ->assertViewHas('allClaim');
+		// $response = $response->call('GET','/home');
+		
+		// $this->assertArrayHasKey('allClaim',$response->getOriginalContent());
+    }
+}
