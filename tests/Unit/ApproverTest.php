76b3e1ef9f4c197233436daffda3d96296ec7902
Muhammad Ghozi <Muhammad Ghozi>
2017-04-16 00:50:41 +0700

    approver test: masalah testcase belum selesai

--- tests/Unit/ApproverTest.php
+++ tests/Unit/ApproverTest.php
@@ -11,35 +11,65 @@ use App\Http\Controllers\ApproverController;
 
 class ApproverTest extends TestCase
 {
-    $this ->be(User::where('role','=','approver')->first());
-    $claim = Claim::where('approver_id','=',$this->id)->where('claim_status','=','1')->first();
+    use DatabaseTransactions;
+
+    $user = User::where('role','=','approver')->first();
+    $claim = Claim::where('approver_id', '=', $this->user->id)->where('claim_status', '=', '1')->first();
     $ac = new ApproverController();
 
+    public function testApprover()
+    {
+      $id=$this->claim->id;
+      $this->approveTest($id);
+      $this->rejectTest($id);
+      $this->showDetailTest($id);
+    }
     public function approveTest($idClaim)
     {
-      $response = $ac->approve($idClaim);
-      $response -> assertRedirect('/home/approver/received');
-
+        $this->actingAs($this->user)
+             ->withSession(['user' => $this->user]);
+        $response = $this->ac->approve($idClaim);
+        $response->assertRedirect('/home/approver/received');
+        $status = $this->claim->claim_status;
+        $approve = 2;
+        $this->assertEquals($status,$approve);
     }
     public function rejectTest($idClaim)
     {
-      $response = $ac->approve($idClaim);
-      $response -> assertRedirect('/home/approver/received');
+        $this->actingAs($this->user)
+             ->withSession(['user' => $this->user]);
+        $response = $this->ac->approve($idClaim);
+        $response->assertRedirect('/home/approver/received');
+        $status = $this->claim->claim_status;
+        $reject = 6;
+        $this->assertEquals($status,$reject);
     }
-    public function showReceivedTest()
+    public function testShowReceived()
     {
-
+        $this->actingAs($this->user)
+             ->withSession(['user' => $this->user]);
+        $response=$this->ac->showReceived();
+        $this->assertInstanceOf('\Illuminate\View\View', $response);
     }
-    public function showApprovedTest()
+    public function testShowApproved()
     {
-
+        $this->actingAs($this->user)
+             ->withSession(['user' => $this->user]);
+        $response=$this->ac->showApproved();
+        $this->assertInstanceOf('\Illuminate\View\View', $response);
     }
-    public function showRejectedTest()
+    public function testShowRejected()
     {
-
+        $this->actingAs($this->user)
+             ->withSession(['user' => $this->user]);
+        $response=$this->ac->showRejected();
+        $this->assertInstanceOf('\Illuminate\View\View', $response);
     }
     public function showDetailTest($idClaim)
     {
-
+        $this->actingAs($this->user)
+             ->withSession(['user' => $this->user]);
+        $response = $this->ac->show($idClaim);
+        $this->assertInstanceOf('\Illuminate\View\View', $response);
     }
 }
