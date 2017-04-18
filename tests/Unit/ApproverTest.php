debc9d842f544e0bc8d129d281168db80bbe0fb9
ZakyKh <mzakykhairuddin@gmail.com>
2017-04-18 16:40:31 +0700

    Add test for function that returns 403 and 404 in ApproverController

--- tests/Unit/ApproverTest.php
+++ tests/Unit/ApproverTest.php
@@ -9,18 +9,21 @@ use App\User;
 use App\Claim;
 use App\Company;
 use App\Http\Controllers\ApproverController;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ApproverTest extends TestCase
 {
     use DatabaseTransactions;
 
     private $ac;
-    private $approver;
+    private $approver1;
+    private $approver2;
     private $claimer;
     private $finance;
     private $sentClaim;
     private $approvedClaim;
     private $rejectedClaim;
+	private $toDeleteClaim;
     private $company;
 
     public function setUp()
@@ -35,14 +38,16 @@ class ApproverTest extends TestCase
           $this->company = $this->makeCompany('company1');
           $company = $this->company;
           $this->claimer = $this->makeUser('claimer1','claimerr1@email.com',$company->id,'claimer');
-          $this->approver = $this->makeUser('approver1','approver1@email.com',$company->id,'approver');
+          $this->approver1 = $this->makeUser('approver1','approver1@email.com',$company->id,'approver');
+          $this->approver2 = $this->makeUser('approver2','approver2@email.com',$company->id,'approver');
           $this->finance = $this->makeUser('finance1','finance1@email.com',$company->id,'finance');
           $idClaimer = $this->claimer->id;
-          $idApprover = $this->approver->id;
+          $idApprover = $this->approver1->id;
           $idFinance = $this->finance->id;
           $this->sentClaim = $this->makeClaim('1',$idClaimer,$idApprover,$idFinance,'1');
           $this->approvedClaim = $this->makeClaim('1',$idClaimer,$idApprover,$idFinance,'2');
           $this->rejectedClaim = $this->makeClaim('1',$idClaimer,$idApprover,$idFinance,'6');
+          $this->toDeleteClaim = $this->makeClaim('1',$idClaimer,$idApprover,$idFinance,'1');
     }
 
     private function makeCompany($name)
@@ -71,10 +76,10 @@ class ApproverTest extends TestCase
 		     ]);
 	  }
 
-    public function testApproved()
+    public function testApprove()
     {
-        $this->actingAs($this->approver)
-             ->withSession(['user' => $this->approver]);
+        $this->actingAs($this->approver1)
+             ->withSession(['user' => $this->approver1]);
         $idClaim = $this->approvedClaim->id;
         $response = $this->ac->approve($idClaim);
         //$status = $this->approvedClaim->claim_status;
@@ -82,10 +87,11 @@ class ApproverTest extends TestCase
         //$this->assertEquals($status,$approve);
         $this->assertDatabaseHas('claims',['id'=>$idClaim,'claim_status'=>$approve]);
     }
+	
     public function testReject()
     {
-       $this->actingAs($this->approver)
-           ->withSession(['user' => $this->approver]);
+       $this->actingAs($this->approver1)
+           ->withSession(['user' => $this->approver1]);
         $idClaim = $this->rejectedClaim->id;
         $response = $this->ac->reject($idClaim);
         //$status = $this->rejectedClaim->claim_status;
@@ -93,20 +99,62 @@ class ApproverTest extends TestCase
         $this->assertDatabaseHas('claims',['id'=>$idClaim,'claim_status'=>$reject]);
         //$this->assertEquals($status,$reject);
     }
-    public function testShowDetail()
+	
+	public function testApproveFail()
     {
-      $this->actingAs($this->approver)
-           ->withSession(['user' => $this->approver]);
+        $this->actingAs($this->approver2)
+             ->withSession(['user' => $this->approver2]);
+        $idClaim = $this->approvedClaim->id;
+		try {
+			$response = $this->ac->approve($idClaim);
+		}
+		catch (HttpException $he) {
+			$this->assertEquals(403,$he->getStatusCode());
+		}
+    }
+	
+	public function testRejectFail()
+    {
+        $this->actingAs($this->approver2)
+             ->withSession(['user' => $this->approver2]);
+        $idClaim = $this->approvedClaim->id;
+		try {
+			$response = $this->ac->reject($idClaim);
+		}
+		catch (HttpException $he) {
+			$this->assertEquals(403,$he->getStatusCode());
+		}
+    }
+	
+    public function testShowExisting()
+    {
+      $this->actingAs($this->approver1)
+           ->withSession(['user' => $this->approver1]);
       $id = $this->sentClaim->id;
       $response = $this->ac->show($id);
       $data = $response->getData();
       $claim = $data['detailClaim'][0];
       $this->assertEquals($claim->id, $id);
     }
+	
+	public function testShowNonexisting()
+	{
+		$this->actingAs($this->approver1)
+			 ->withSession(['user' => $this->approver1]);
+		$id = $this->toDeleteClaim->id;
+		$this->toDeleteClaim->delete();
+		try {
+			$this->ac->show($id);
+		}
+		catch (HttpException $he) {
+			$this->assertEquals(404,$he->getStatusCode());
+		}
+	}
+	
     public function testShowReceived()
     {
-        $this->actingAs($this->approver)
-             ->withSession(['user' => $this->approver]);
+        $this->actingAs($this->approver1)
+             ->withSession(['user' => $this->approver1]);
         $response=$this->ac->showReceived();
         $data = $response->getData();
         $retrievedClaim = $data['claims'][0];
@@ -114,8 +162,8 @@ class ApproverTest extends TestCase
     }
     public function testShowApproved()
     {
-        $this->actingAs($this->approver)
-           ->withSession(['user' => $this->approver]);
+        $this->actingAs($this->approver1)
+           ->withSession(['user' => $this->approver1]);
         $response=$this->ac->showApproved();
         $data = $response->getData();
         $retrievedClaim = $data['claims'][0];
@@ -123,8 +171,8 @@ class ApproverTest extends TestCase
     }
     public function testShowRejected()
     {
-        $this->actingAs($this->approver)
-           ->withSession(['user' => $this->approver]);
+        $this->actingAs($this->approver1)
+           ->withSession(['user' => $this->approver1]);
         $response=$this->ac->showRejected();
         $data = $response->getData();
         $retrievedClaim = $data['claims'][0];
