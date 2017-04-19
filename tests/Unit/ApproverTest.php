3f567c7658cfd3b4cbeb2a7581e31038bca80965
ZakyKh <mzakykhairuddin@gmail.com>
2017-04-19 20:50:35 +0700

    Fix some indentation and mismatched data type

--- tests/Unit/ApproverTest.php
+++ tests/Unit/ApproverTest.php
@@ -27,54 +27,55 @@ class ApproverTest extends TestCase
     private $company;
 
     public function setUp()
-    {
-      parent::setUp();
-      $this->generateTestData();
-    }
+	{
+		parent::setUp();
+		$this->generateTestData();
+	}
 
     public function generateTestData()
-    {
-          $this->ac = new ApproverController();
-          $this->company = $this->makeCompany('company1');
-          $company = $this->company;
-          $this->claimer = $this->makeUser('claimer1','claimerr1@email.com',$company->id,'claimer');
-          $this->approver1 = $this->makeUser('approver1','approver1@email.com',$company->id,'approver');
-          $this->approver2 = $this->makeUser('approver2','approver2@email.com',$company->id,'approver');
-          $this->finance = $this->makeUser('finance1','finance1@email.com',$company->id,'finance');
-          $idClaimer = $this->claimer->id;
-          $idApprover = $this->approver1->id;
-          $idFinance = $this->finance->id;
-          $this->sentClaim = $this->makeClaim('1',$idClaimer,$idApprover,$idFinance,'1');
-          $this->approvedClaim = $this->makeClaim('1',$idClaimer,$idApprover,$idFinance,'2');
-          $this->rejectedClaim = $this->makeClaim('1',$idClaimer,$idApprover,$idFinance,'6');
-          $this->toDeleteClaim = $this->makeClaim('1',$idClaimer,$idApprover,$idFinance,'1');
-    }
+	{
+		$this->ac = new ApproverController();
+		$this->company = $this->makeCompany('company1');
+		$company = $this->company;
+		$this->claimer = $this->makeUser('claimer1','claimerr1@email.com',$company->id,'claimer');
+		$this->approver1 = $this->makeUser('approver1','approver1@email.com',$company->id,'approver');
+		$this->approver2 = $this->makeUser('approver2','approver2@email.com',$company->id,'approver');
+		$this->finance = $this->makeUser('finance1','finance1@email.com',$company->id,'finance');
+		$idClaimer = $this->claimer->id;
+		$idApprover = $this->approver1->id;
+		$idFinance = $this->finance->id;
+		$this->sentClaim = $this->makeClaim(1,$idClaimer,$idApprover,$idFinance,1);
+		$this->approvedClaim = $this->makeClaim(1,$idClaimer,$idApprover,$idFinance,1);
+		$this->rejectedClaim = $this->makeClaim(1,$idClaimer,$idApprover,$idFinance,1);
+		$this->toDeleteClaim = $this->makeClaim(1,$idClaimer,$idApprover,$idFinance,1);
+	}
 
     private function makeCompany($name)
-    {
-          return factory(Company::class)->create([
-              'name' => $name
-          ]);
-    }
+	{
+		return factory(Company::class)->create([
+			'name' => $name
+		]);
+	}
+	
     public function makeUser($name, $email, $company, $role)
-	  {
-		     return factory(User::class)->create([
-			        'name' => $name,
-			        'email' => $email,
-			        'company' => $company,
-			        'role' => $role,
-		     ]);
-	  }
+	{
+		return factory(User::class)->create([
+			'name' => $name,
+			'email' => $email,
+			'company' => $company,
+			'role' => $role,
+		]);
+	}
     public function makeClaim($claim_type, $claimer_id, $approver_id,$finance_id,$claim_status)
-	   {
-		     return factory(Claim::class)->create([
-           'claim_type' => $claim_type,
-           'claimer_id' => $claimer_id,
-           'approver_id' => $approver_id,
-           'finance_id' => $finance_id,
-           'claim_status' => $claim_status,
-		     ]);
-	  }
+	{
+		return factory(Claim::class)->create([
+			'claim_type' => $claim_type,
+			'claimer_id' => $claimer_id,
+			'approver_id' => $approver_id,
+			'finance_id' => $finance_id,
+			'claim_status' => $claim_status,
+		]);
+	}
 
     public function testApprove()
     {
