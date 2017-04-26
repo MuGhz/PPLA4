a8381a6a7e37266dce45fdf2eb91439c13a9934e
Muhammad Ghozi <Muhammad Ghozi>
2017-04-26 20:12:10 +0700

    [RED] test order hotel

--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -29,13 +29,16 @@ class OrderTest extends TestCase
      protected $token;
      protected $hotelList;
      protected $hotelDetail;
-
+     proctected $orderJson;
     public function setUp()
     {
         parent::setUp();
         $this->token = '{"diagnostic":{"status":200},"output_type":"json","token":"3b00ae1956bac189967cfff807fff929c4e6415b"}';
         $this->hotelList = '{"diagnostic":{"status":200},"output_type":"json","search_queries":{"q":"jakarta","uid":"","startdate":"2017-04-20","enddate":"2017-04-21","night":"1","room":1,"adult":"1","child":0,"sort":false,"minstar":0,"maxstar":5,"minprice":"213000.00","maxprice":"1000000000.00","distance":100000},"results":{"result":[{"province_name":"DKI Jakarta","kecamatan_name":"Gambir","kelurahan_name":"Petojo Selatan","business_uri":"https:\/\/api-sandbox.tiket.com\/hotel\/indonesia\/jakarta\/jakarta\/neo-hotel-cideng-jakarta?startdate=2017-04-20&night=1&room=1&adult=1&child=0&is_partner=0&star_rating%5B0%5D=0&star_rating%5B1%5D=1&star_rating%5B2%5D=2&star_rating%5B3%5D=3&star_rating%5B4%5D=4&star_rating%5B5%5D=5&hotel_chain=0&facilities=0&latitude=0&longitude=0&distance=0&uid=business%3A222778",hotel_id":"21427344"}]},"pagination":{"total_found":62,"current_page":"1","offset":"20","lastPage":4},"login_status":"false","token":"25599b029a0c3e3a87bde80091cf15bed064c099"}';
         $this->hotelDetail = '{"diagnostic":{"status":200},"output_type":"json","primaryPhotos":"https:\/\/sandbox.tiket.com\/img\/business\/f\/a\/business-favehotel-tanah-abang-cideng-jakarta-hotel-jakarta-pusat2677.s.jpg","breadcrumb":{"business_id":"222778","business_uri":"https:\/\/api-sandbox.tiket.com\/neo-hotel-cideng-jakarta","business_name":"Favehotel Tanah Abang","area_id":"87528","area_name":"Monas","kelurahan_id":"69","kelurahan_name":"Petojo Selatan","kecamatan_id":"19","kecamatan_name":"Gambir","city_id":"21681","city_name":"Jakarta","province_id":"13","province_name":"DKI Jakarta","country_id":"id","country_name":"Indonesia","continent_id":"1","continent_name":"Asia","kelurahan_uri":"https:\/\/api-sandbox.tiket.com\/search\/hotel?uid=city:21681"},"login_status":"false","token":"c8d2e6aa8d84d93cde61fdc9d63e6215f6a1ef69"}';
+        $this->orderJson = '{"diagnostic":{"status":200,"elapsetime":"0.3230","memoryusage":"23.39MB","unix_timestamp":1493200252,"confirm":"success","lang":"id","currency":"IDR"},"output_type":"json",
+        "myorder":{"order_id":"33440460","data":[],"total":0,"total_tax":0,"total_without_tax":0,"count_installment":0,"discount":0,"discount_amount":0},"checkout":"https:\/\/api-sandbox.tiket.com\/order\/checkout\/33440460\/IDR",
+        "login_status":"false","token":"6bf08ec096529ad7d610ab9c1a2739892b0b2311"}';
     }
 
     private function makeCompany($name)
@@ -65,7 +68,6 @@ class OrderTest extends TestCase
 			        'claim_status' => $claim_status,
 		          ]);
 	    }
-
 
     public function curlMock($returnValue)
     {
@@ -192,6 +194,19 @@ class OrderTest extends TestCase
         $oc->rebookHotel($claim->id);
         $difference = $claim->updated_at->diff($now)->days;
         $this->assertEquals(0,$difference);
+    }
+
+    public function testOrderHotel()
+    {
+        $order = $this->curlMock($this->orderJson);
+
+        $company = $this->makeCompany('Test Company');
+        $claimer = $this->makeUser('Claimer 1', 'Claimer1@Company.test', $company->id, 'claimer');
+        $approver = $this->makeUser('Approver', 'Appover@Company.test', $company->id, 'approver');
+        $finance = $this->makeUser('Finance', 'Finance@Company.test', $company->id, 'finance');
+        $claim = $this->makeClaim(1,$claimer->id,$approver->id,$finance->id,2);
+        $response = $order->orderHotel($claim->id);
+        $this->assertEquals($response,$this->orderJson);
     }
 
     public function testBookHotel()
