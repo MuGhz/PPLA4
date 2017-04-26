68e7859dd27d9be4adcee13643001159e564f2d8
gwahidt <gwahidt@gmail.com>
2017-04-26 17:51:00 +0700

    [GREEN] nambah tombol buat reject finance/approver

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -89,6 +89,7 @@ class OrderController extends Controller
             $claim->approver_id = User::approver($claimer)->id;
             $claim->finance_id = User::finance($claimer)->id;
             $claim->claim_status = 1;
+            $claim->order_information = 'test';
             $claim->save();
         }
 
