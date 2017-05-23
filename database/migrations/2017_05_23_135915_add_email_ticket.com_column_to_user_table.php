80624f0aff7c4e96141ced0d2cb71c79cd9129ba
Muhammad Ghozi <Muhammad Ghozi>
2017-05-23 21:02:04 +0700

    migrate again add email for ticket.com

--- database/migrations/2017_05_23_135915_add_email_ticket.com_column_to_user_table.php
+++ database/migrations/2017_05_23_135915_add_email_ticket.com_column_to_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddOrderIdColumnToClaimTable extends Migration
+class AddEmailTicket.comColumnToUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AddOrderIdColumnToClaimTable extends Migration
      */
     public function up()
     {
-        Schema::table('claims', function (Blueprint $table) {
-            $table -> string('order_id')->nullable();
+        Schema::table('users', function (Blueprint $table) {
+            $table -> string('email_ticket')->nullable();
         });
     }
 
@@ -25,8 +25,8 @@ class AddOrderIdColumnToClaimTable extends Migration
      */
     public function down()
     {
-        Schema::table('claims', function (Blueprint $table) {
-            $table->dropColumn('order_id');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email_ticket');
         });
     }
 }
